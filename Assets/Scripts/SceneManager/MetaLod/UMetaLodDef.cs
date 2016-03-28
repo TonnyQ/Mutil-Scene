85387b4868bb92287c5e1e9fb6ad9e24b38d76fa
TonnyQ <tonny_2014@yeah.net>
2016-03-29 01:11:01 +0800

    Revert "Revert "#00001 四叉树与广义Lod实现代码""
    This reverts commit 71d006db594b4f18d17181526f29488b12cee9b9.

---
+++ Assets/Scripts/SceneManager/MetaLod/UMetaLodDef.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace umlod
+{
+    public delegate float fnFactorNormalize(float value, float upper, float lower);
+    public delegate void fnHeatAttenuate(float fps, out float distInner, out float distOuter);
+
+    public struct UImpactFactor
+    {
+        public string Name;
+        public float Weight;
+        public float UpperBound;
+        public float LowerBound;
+        public fnFactorNormalize Normalizer;
+    }
+
+    public enum UPerfLevel
+    {
+        Highend,
+        Medium,
+        Lowend,
+    }
+
+    public class UMetaLodConst
+    {
+        // the bounding volume of the target
+        public const string Factor_Bounds = "Bounds";
+
+        // currently corresponds to vertex count of the target mesh, would be 0 for particle system
+        public const string Factor_GeomComplexity = "GeomComplexity";
+
+        // currently correspends to particle count of the target particle system, would be 0 for ordinary mesh
+        public const string Factor_PSysComplexity = "PSysComplexity";
+
+        // a subjective factor which reveals the visual importance of the target in some degrees
+        // for instance, skill effects casted by player would generally has a 
+        // pretty much higher visual impact than a static stone on the ground
+        public const string Factor_VisualImpact = "VisualImpact";
+    }
+
+    public class UMetaLodUtil
+    {
+        public static float Percent(float from, float to, float value)
+        {
+            if (float.Equals(from, to))
+                return 0.0f;
+
+            float clamped = Mathf.Clamp(value, from, to);
+            return (clamped - from) / (to - from);
+        }
+    }
+
+    public class UMetaLodDefaults
+    {
+        public static UImpactFactor Factor_Bounds = new UImpactFactor()
+        {
+            Name = UMetaLodConst.Factor_Bounds,
+            Weight = 0.2f,
+            UpperBound = 20.0f,
+            LowerBound = 5.0f,
+            Normalizer = (value, upper, lower) => { return UMetaLodUtil.Percent(lower, upper, value); },
+        };
+
+        public static UImpactFactor Factor_GeomComplexity = new UImpactFactor()
+        {
+            Name = UMetaLodConst.Factor_GeomComplexity,
+            Weight = 0.05f,
+            UpperBound = 1.0f,
+            LowerBound = 0.0f,
+            Normalizer = (value, upper, lower) => { return UMetaLodUtil.Percent(lower, upper, value); },
+        };
+
+        public static UImpactFactor Factor_PSysComplexity = new UImpactFactor()
+        {
+            Name = UMetaLodConst.Factor_PSysComplexity,
+            Weight = 0.05f,
+            UpperBound = 1.0f,
+            LowerBound = 0.0f,
+            Normalizer = (value, upper, lower) => { return UMetaLodUtil.Percent(lower, upper, value); },
+        };
+
+        public static UImpactFactor Factor_VisualImpact = new UImpactFactor()
+        {
+            Name = UMetaLodConst.Factor_VisualImpact,
+            Weight = 0.1f,
+            UpperBound = 1.0f,
+            LowerBound = 0.0f,
+            Normalizer = (value, upper, lower) => { return UMetaLodUtil.Percent(lower, upper, value); },
+        };
+
+        public static fnHeatAttenuate HeatAttenuation = (float fps, out float inner, out float outer) =>
+        {
+            float fpsClamped = Mathf.Clamp(fps, UMetaLodConfig.FpsLowerBound, UMetaLodConfig.FpsUpperBound);
+            float fpsScale = 0.0f;
+            if (fpsClamped < UMetaLodConfig.FpsStandard)
+            {
+                fpsScale = UMetaLodConfig.FpsMinifyFactor * (1.0f - UMetaLodUtil.Percent(UMetaLodConfig.FpsLowerBound, UMetaLodConfig.FpsStandard, fpsClamped));
+            }
+            else
+            {
+                fpsScale = UMetaLodConfig.FpsMagnifyFactor * (1.0f - UMetaLodUtil.Percent(UMetaLodConfig.FpsStandard, UMetaLodConfig.FpsUpperBound, fpsClamped));
+            }
+
+            float overallScale = 1.0f + UMetaLodConfig.PerfLevelScaleLut[UMetaLodConfig.PerformanceLevel] + fpsScale;
+            inner = UMetaLodConfig.DistInnerBound * overallScale;
+            outer = UMetaLodConfig.DistOuterBound * overallScale;
+        };
+    }
+}
+
---
+++ Assets/TestFullScene/UTestBootstrap1.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class UTestBootstrap : MonoBehaviour
+{
+    UTestQuadtree m_testbed = new UTestQuadtree();
+
+    GameObject m_player = null;
+    GameObject m_moveTarget = null;
+
+    bool _alwaysMove = false;
+    bool _drawDebugLines = false;
+
+    void Start()
+    {
+        m_testbed.Init();
+        m_player = GameObject.Find("Player");
+        m_moveTarget = GameObject.Find("MoveTarget");
+    }
+
+    void Update()
+    {
+        if (_drawDebugLines)
+            UCore.DrawRect(m_testbed.Bound, 0.1f, Color.white);
+
+        Vector3 target = m_moveTarget.transform.position;
+        Vector3 dist = target - m_player.transform.position;
+        if (dist.magnitude > 1.0f)
+        {
+            Vector3 delta = dist.normalized * Time.deltaTime * 100.0f;
+            if (delta.magnitude > dist.magnitude)
+            {
+                m_player.transform.position = target;
+            }
+            else
+            {
+                m_player.transform.position += delta;
+            }
+        }
+        else
+        {
+            if (_alwaysMove)
+            {
+                SetNewTarget();
+            }
+        }
+
+        m_testbed.Update(m_player.transform.position);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, Screen.height * 0.1f - 40, 80, 40), "Move"))
+        {
+            SetNewTarget();
+        }
+
+        _alwaysMove = GUI.Toggle(new Rect(50, Screen.height * 0.2f, 100, 20), _alwaysMove, "Always Move");
+        _drawDebugLines = GUI.Toggle(new Rect(50, Screen.height * 0.25f, 100, 20), _drawDebugLines, "Debug Lines");
+
+        if (_drawDebugLines != m_testbed.Quadtree.EnableDebugLines)
+            m_testbed.Quadtree.EnableDebugLines = _drawDebugLines;
+    }
+
+    void SetNewTarget()
+    {
+        m_moveTarget.transform.position = m_testbed.NewRandomPoint(0.8f);
+    }
+}
