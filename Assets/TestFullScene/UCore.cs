72a675151af1f2a1bd18522d4b0015dcb5d52921
TonnyQ <tonny_2014@yeah.net>
2016-03-29 00:48:17 +0800

    #00001 四叉树与广义Lod实现代码
    引入四叉树与LoD方案

---
+++ Assets/TestFullScene/UCore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UCore
+{
+    public static void Assert(bool expr)
+    {
+        System.Diagnostics.Debug.Assert(expr);
+    }
+
+    public static void DrawRect(Rect r, float y, Color c, float padding = 0.0f)
+    {
+        Debug.DrawLine(new Vector3(r.xMin + padding, y, r.yMin + padding), new Vector3(r.xMin + padding, y, r.yMax - padding), c);
+        Debug.DrawLine(new Vector3(r.xMin + padding, y, r.yMin + padding), new Vector3(r.xMax - padding, y, r.yMin + padding), c);
+        Debug.DrawLine(new Vector3(r.xMax - padding, y, r.yMax - padding), new Vector3(r.xMin + padding, y, r.yMax - padding), c);
+        Debug.DrawLine(new Vector3(r.xMax - padding, y, r.yMax - padding), new Vector3(r.xMax - padding, y, r.yMin + padding), c);
+    }
+}
+
