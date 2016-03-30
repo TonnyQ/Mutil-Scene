09781d85f226dc59015ba20d0b71f48cf729ec68
TonnyQ <tonny_2014@yeah.net>
2016-03-30 20:47:58 +0800

    #000010 添加一个Mono版的Singleton类

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    private static object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            if (applicationIsQuitting)
+            {    
+                return null;
+            }
+
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    _instance = singleton.AddComponent<T>();
+                    singleton.name = typeof(T).ToString();
+
+                    DontDestroyOnLoad(singleton);
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    private static bool applicationIsQuitting = false;
+   
+    public void OnDestroy()
+    {
+        applicationIsQuitting = true;
+    }
+}
